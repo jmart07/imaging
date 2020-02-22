506c55d883eec0cc90a60e100b6765c6df93694f
John Martin <jmart07@gmail.com>
2020-02-21 22:09:49 -0500

    loaded axios, console logged succesful get request

--- imaging-client/src/App.js
+++ imaging-client/src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+import Menu from './Menu.js';
+import Display from './Display.js';
 
-function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    this.getStudents();
+  }
+
+  getStudents = () => {
+    axios.get('http://localhost:3000/students')
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Menu />
+        <Display />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ imaging-client/src/Display.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Display extends React.Component {
+  render() {
+    return(
+      <div>display</div>
+    )
+  }
+}
+
+export default Display;
