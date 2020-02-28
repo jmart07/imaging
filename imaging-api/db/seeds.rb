f6c065184b54ecb3f29535a4d31246d136b78ad7
John Martin <jmart07@gmail.com>
2020-02-28 13:33:46 -0500

    configured new rails models with relationship

--- imaging-api/db/seeds.rb
+++ imaging-api/db/seeds.rb
@@ -8,7 +8,7 @@
 
 require 'faker'
 
-p "Seeding database"
+p "Seeding students"
 
 10.times { |i|
     Student.create({
@@ -32,16 +32,66 @@ p "Seeding database"
     })
 }
 
+p "Seeding templates"
+
+Template.create({name: "Bad guys"})
+Template.create({name: "Good guys"})
+
+p "Seeding shapes"
+
+shapes = [
+    {
+      id: 1,
+      type: 'idNumber',
+      x: 390,
+      y: 120,
+      text: '1234567',
+      fontSize: 30
+    },
+    {
+      id: 1,
+      type: 'photo',
+      x: 80,
+      y: 60,
+      width: 230,
+      height: 230
+    },
+    {
+      id: 1,
+      type: 'name',
+      x: 390,
+      y: 90,
+      text: 'John Martin',
+      fontSize: 30
+    },
+    {
+      id: 1,
+      type: 'exportId',
+      x: 390,
+      y: 150,
+      text: 'EXPORTID',
+      fontSize: 30
+    },
+    {
+      id: 1,
+      type: 'barcode',
+      x: 130,
+      y: 300,
+      text: 'BARCODE',
+      fontSize: 100
+    },
+    {
+      id: 1,
+      type: 'grade',
+      x: 390,
+      y: 240,
+      text: 'Class of 2020',
+      fontSize: 30
+    }
+]
+
+shapes.each {|shape|
+    Shape.create({shape})
+}
+
 p "Done seeding"
-
-
-# graduation: {[
-#     '2020-6-15',
-#     '2021-6-15',
-#     '2022-6-15',
-#     '2023-6-15',
-#     '2024-6-15',
-#     '2025-6-15',
-#     '2026-6-15',
-#     '2027-6-15',
-# ].sample()}
