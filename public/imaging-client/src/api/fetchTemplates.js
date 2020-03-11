de1edb17d8bd0a3cb4c97ef65ab3d680b540f621
unknown <jmart07@gmail.com>
2020-03-11 17:43:13 -0400

    edited package.jsons and API calls

--- public/imaging-client/src/api/fetchTemplates.js
+++ public/imaging-client/src/api/fetchTemplates.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const fetchTemplates = async () => {
-  return await axios.get('http://localhost:3000/templates')
+  return await axios.get('', data)
   .then((response) => {
     console.log('response',response.data)
     return response.data})
