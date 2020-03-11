94bbe92216854bedc40992d3fdfb0f97b65051d9
unknown <jmart07@gmail.com>
2020-03-11 17:42:56 -0400

    edited package.jsons and API calls

--- public/imaging-client/src/api/fetchStudents.js
+++ public/imaging-client/src/api/fetchStudents.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const fetchStudents = async () => {
-  return await axios.get('http://localhost:3000/students')
+  return await axios.get('', data)
   .then((response) => {
     console.log('response',response.data)
     return response.data})
